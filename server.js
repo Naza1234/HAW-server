f979c27ee114ce6cee77965fbe41776efe0b8a2b
Ozurmba chibuike <ocbmf17@gmail.com>
2024-01-30 03:51:26 +0100

    updated link

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 
 const corsOptions = {
-  origin: 'https://autoauction.space',
+  origin: 'https://equiauction.space',
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
 };
 
