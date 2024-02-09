9d96dc00b3260c276e2d1a2ccc10eca565860e5c
Ozurmba chibuike <ocbmf17@gmail.com>
2024-02-09 09:08:54 +0100

    done
    done

--- routes/productImageRoutes.js
+++ routes/productImageRoutes.js
@@ -3,8 +3,8 @@ const router = express.Router();
 const productImageController = require('../controllers/productImageController');
 
 // Routes for product image operations
-router.post('/products/:productId/images',productImageController.upload, productImageController.createProductImage);
-router.get('/products/:productId/images', productImageController.getProductImagesByProductId);
+router.post('/products/images/:productId',productImageController.upload, productImageController.createProductImage);
+router.get('/products/images/:productId', productImageController.getProductImagesByProductId);
 router.get('/productImages/:id', productImageController.getSingleProductImage);
 router.put('/productImages/:id', productImageController.updateSingleProductImage);
 router.delete('/productImages/:id', productImageController.deleteSingleProductImage);
--- routes/videoRoutes.js
+++ routes/videoRoutes.js
@@ -4,7 +4,7 @@ const videoController = require('../controllers/videoController');
 
 // Routes for video operations
 router.post('/videos',videoController.upload, videoController.createVideo);
-router.get('/products/:productId/videos', videoController.getVideosByProductId);
+router.get('/products/videos/:productId', videoController.getVideosByProductId);
 router.get('/videos/:id', videoController.getSingleVideo);
 router.put('/videos/:id', videoController.updateSingleVideo);
 router.delete('/videos/:id', videoController.deleteSingleVideo);
